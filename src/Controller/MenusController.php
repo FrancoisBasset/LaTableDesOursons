9e6f57f085229086cab7f645c5daf21d064e31b0
FrancoisBasset <francois@local>
2021-12-22 11:54:54 +0100

    maj

--- src/Controller/MenusController.php
+++ src/Controller/MenusController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,9 +11,9 @@ class MenusController extends AbstractController
     /**
 	 * @Route("/menus", name="menus")
 	 */
-    public function index(MenuRepository $menuRepository): Response
+    public function index(): Response
     {
-		$menus = $menuRepository->findAll();
+		$menus = [];//$menuRepository->findAll();
 
         return $this->render('menus/index.html.twig', [
 			'menus' => $menus
