2265d10ce2f35c20ee82c937e5c9692112fc1d39
FrancoisBasset <francois@local>
2021-12-22 21:51:58 +0100

    maj

---
+++ src/Controller/CommanderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommanderController extends AbstractController
+{
+    /**
+	 * @Route("/commander", name="commander")
+	 */
+    public function index(): Response
+    {
+        return $this->render('commander/index.html.twig');
+    }
+}
