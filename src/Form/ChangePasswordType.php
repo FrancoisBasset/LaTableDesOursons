549621d16c98e49060fe9df9673e684b5696d962
FrancoisBasset <francois@local>
2022-01-18 14:33:09 +0100

    maj

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ChangePasswordType extends AbstractType
@@ -11,7 +12,10 @@ class ChangePasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', PasswordType::class)
+            ->add('password', RepeatedType::class, [
+				'type' => PasswordType::class,
+				'invalid_message' => 'Les 2 champs ne sont pas identiques'
+			])
         ;
     }
 }
