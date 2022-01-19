078205412684520e3dafc43640ed6a0edfa6ed6e
FrancoisBasset <francois@local>
2022-01-19 10:18:48 +0100

    maj

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+		if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
+			return $this->redirectToRoute('admin');
+		}
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
